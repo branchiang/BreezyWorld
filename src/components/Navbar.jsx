1b37bf62e2fd25348e2bd6bb2e1170d816360879
tmaz0003 <tmaz0003@student.monash.edu>
2022-08-11 19:15:30 +0200

    Navbar Update 1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Conatainer = styled.div`
+    height: 60px;
+    width: 100px;
+    background-color: black;
+`
+
+const Navbar = () => {
+    return (
+        <Conatainer>
+            Navbar
+        </Conatainer>
+    )
+}
+
+export default Navbar
+
