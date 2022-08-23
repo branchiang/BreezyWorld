6779c52077c1b87c822c391fcb34124e19fc0801
branchiang <brandonchiang69@gmail.com>
2022-08-23 20:58:45 +0200

    hero1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,39 +3,64 @@ import styled from "styled-components"
 const Container = styled.div`
 width: 100%;
 height: 100vh;
-`
+display: flex;
+position: relative;
+overflow: hidden;
+` ;
+
 const HeroContainer = styled.div`
+width: 100vw;
+height: 100vh;
 display: flex;
 align-items: center;
 ` ;
 
 const HeroImg = styled.div`
+height: 100%;
 flex: 1;
 ` ;
 
 const HeroInfo = styled.div`
 flex: 1;
+padding: 50px;
 ` ;
 
-const Image = styled.image``;
+const Image = styled.img`
+height: 80%;
+`;
 
-const Title = styled.h1``
-const Desc = styled.p``
-const Button = styled.button``
+const Title = styled.h1`
+font-size: 70px;
+`;
+
+const Desc = styled.p`
+margin: 50px 0px;
+font-size: 20px;
+font-weight: 500;
+letter-spacing: 3px;
+` ;
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+` ;
 
 
 
 const Hero = () => {
   return (
     <Container>
+      <HeroContainer>
         <HeroImg>
-          <Image/>
+          <Image src="/heroImg.jpg" />
         </HeroImg>
         <HeroInfo>
           <Title>WELCOME</Title>
           <Desc>Real comfort, visual and physical, is vital to every room </Desc>
           <Button>SHOP NOW</Button>
         </HeroInfo>
+        </HeroContainer>
     </Container>
   )
 }
