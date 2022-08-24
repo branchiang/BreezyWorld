052240e4861d25401e7c4a21e4158041eb486738
branchiang <brandonchiang69@gmail.com>
2022-08-24 03:40:46 +0200

    hero2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,67 +1,93 @@
-import styled from "styled-components"
-
-const Container = styled.div`
-width: 100%;
-height: 100vh;
-display: flex;
-position: relative;
-overflow: hidden;
-` ;
+import React from 'react'
+import styled from 'styled-components'
+import HeroBgImg from '../../src/heroImg.jpg'
 
 const HeroContainer = styled.div`
-width: 100vw;
-height: 100vh;
-display: flex;
-align-items: center;
-` ;
+  background: #0c0c0c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 100vh;
+  position: relative;
+  z-index: 1;
+`;
 
-const HeroImg = styled.div`
-height: 100%;
-flex: 1;
-` ;
-
-const HeroInfo = styled.div`
-flex: 1;
-padding: 50px;
-` ;
+const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
 
 const Image = styled.img`
-height: 80%;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
 `;
 
-const Title = styled.h1`
-font-size: 70px;
+const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 300px;
+  position: absolute;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const Desc = styled.p`
-margin: 50px 0px;
-font-size: 20px;
-font-weight: 500;
-letter-spacing: 3px;
-` ;
+const HeroH1 = styled.h1`
+  font-size: 70px;
+  text-align: center;
+`;
+
+
+const HeroP = styled.p`
+  margin: 30px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+const HeroBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const Button = styled.button`
-padding: 10px;
-font-size: 20px;
-background-color: transparent;
-cursor: pointer;
-` ;
-
+  padding: 10px;
+  font-size: 20px;
+  background-color: #74482F;
+  color: white;
+  cursor: pointer;
+`;
 
 
 const Hero = () => {
   return (
-    <Container>
-      <HeroContainer>
-        <HeroImg>
-          <Image src="/heroImg.jpg" />
-        </HeroImg>
-        <HeroInfo>
-          <Title>WELCOME</Title>
-          <Desc>Real comfort, visual and physical, is vital to every room </Desc>
-          <Button>SHOP NOW</Button>
-        </HeroInfo>
-        </HeroContainer>
-    </Container>
+    <HeroContainer>
+
+      <HeroBg>
+        <Image src={HeroBgImg}/>
+      </HeroBg>
+
+      <HeroContent>
+        <HeroH1>WELCOME</HeroH1>
+        <HeroP>
+          Real comfort, visual and physical, is vital to every room 
+        </HeroP>
+        <HeroBtn>
+          <Button>Shop Now</Button>
+        </HeroBtn>
+      </HeroContent>
+
+    </HeroContainer>
   )
 }
 
